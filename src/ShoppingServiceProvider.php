3ef824f4c612097ed7a4ff166f9e101eb8a4e06f
Austin Kregel <5355937+austinkregel@users.noreply.github.com>
2022-11-24 23:52:09 -0500

    Update workflow and the config

--- src/ShoppingServiceProvider.php
+++ src/ShoppingServiceProvider.php
@@ -6,11 +6,12 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 use Spork\Core\Spork;
 
-class ShoppingServiceProvider extends ServiceProvider
+class \ShoppingServiceProvider extends ServiceProvider
 {
     public function register()
     {
         Spork::addFeature('shopping', 'ShoppingCartIcon', '/shopping', 'tool');
+        $this->mergeConfigFrom(__DIR__ . '/../config/spork.php', 'spork.shopping');
 
         if (config('spork.shopping.enabled')) {
             Route::prefix('api')->group(__DIR__.'/../routes/api.php');
